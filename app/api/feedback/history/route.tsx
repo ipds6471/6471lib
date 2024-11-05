a3f2e004667975cae5385f8418ebf2188a379c3b
unknown <USER@IPDS-06.bps.go.id>
2024-11-05 08:00:50 +0800

    initial data

--- app/api/feedback/history/route.tsx
+++ app/api/feedback/history/route.tsx
@@ -2,29 +2,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 import { db } from "@vercel/postgres";
-import { error } from "console";
 const client = await db.connect();
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   console.log(req);
-//   if (req.method === "GET") {
-//     try {
-//       const feedback = await client.sql`SELECT * FROM feedback`;
-//       return res.status(200).json({ feedback: feedback.rows });
-//     } catch (err) {
-//       return res.status(500).json({ error: "failed to load data" });
-//     }
-//   }
-// }
 
 export async function GET() {
   try {
     const feedback = await client.sql`SELECT * FROM feedback`;
     return NextResponse.json({ success: true, data: feedback.rows });
   } catch (err) {
+    console.log(err);
     return NextResponse.json({ error: err });
   }
 }
--- app/feedback/history/page.tsx
+++ app/feedback/history/page.tsx
@@ -8,53 +8,16 @@ export default async function Page() {
 
   if (result.success) {
     feedbackData = result.data;
-    console.log(feedbackData);
+    return (
+      <div>
+        <FeedbackTable rows={feedbackData} />
+      </div>
+    );
   }
-
-  console.log(FeedbackTable);
-
-  let da = [
-    {
-      id: "98245f02-e6ad-4a64-8cc0-12383024c9c0",
-      value: 3,
-      created_at: "2024-11-03T20:55:30.236Z",
-      updated_at: null,
-    },
-    {
-      id: "874dea79-e73c-4e58-990e-64ca3e67650b",
-      value: 3,
-      created_at: "2024-11-03T20:58:54.449Z",
-      updated_at: null,
-    },
-    {
-      id: "1efa1e32-c64b-410d-af77-8ee91db40f3c",
-      value: 1,
-      created_at: "2024-11-03T20:59:35.529Z",
-      updated_at: null,
-    },
-    {
-      id: "a1e4a875-9b72-4142-a3d2-2509e914c8e9",
-      value: 2,
-      created_at: "2024-11-03T21:01:47.651Z",
-      updated_at: null,
-    },
-    {
-      id: "fbb1b797-26e1-420f-9089-f595d52c5c44",
-      value: 3,
-      created_at: "2024-11-03T21:03:19.533Z",
-      updated_at: null,
-    },
-    {
-      id: "5e42bcf7-b183-43e8-ab66-42dfb27ccb98",
-      value: 1,
-      created_at: "2024-11-03T21:04:26.711Z",
-      updated_at: null,
-    },
-  ];
 
   return (
     <div>
-      <FeedbackTable rows={da} />
+      <p className="text-danger">Gagal mengambil data</p>
     </div>
   );
 }
